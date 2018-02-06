11ddc66fcc573823d9df4509ea03ee1c07e8ba43
Carlos <csaave2@illinois.edu>
2018-02-06 12:46:33 -0600

    commit lab 3

--- src/urlToString.java
+++ src/urlToString.java
@@ -1,14 +1,15 @@
-public class urlToString {
-    import java.io.IOException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
 
+public class urlToString {
     /**
      * Retrieve contents from a URL and return them as a string.
      *
      * @param url url to retrieve contents from
      * @return the contents from the url as a string, or an empty string on error
      */
+
     public static String urlToString(final String url) {
         Scanner urlScanner;
         try {
